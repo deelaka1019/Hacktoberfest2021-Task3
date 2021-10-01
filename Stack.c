0288166a53e9e780a706f9b1d0f52a0619c89303
deelaka-1 <57798902+deelaka1019@users.noreply.github.com>
2021-10-02 00:22:57 +0530

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -1,109 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
-//Global Variable
-int size,choice,ele;
 
+//Global Variable
+int size, choice, ele, a;
 
 //Creating Stack
-struct stack{
+struct stack {
     int arr[100];
     int top;
 
-}st;
+} st;
 
 //Inserting Element
-void push(int element)
-{
-    if((st.top)==size)
-    {
+void push() {
+    if ((st.top) == (size - 1)) {
         printf("\n Stack is Full");
-    }
-    else
-    {
-        st.top--;
+    } else {
+        st.top++;
         printf("\nEnter a Value ");
-        scanf("%s",&ele);
-        st.arr[st.top]=ele;
+        scanf("%d", &ele);
+        st.arr[st.top] = ele;
     }
 }
 
 //Removing Element
-int pop()
-{
-    if((st.top)==-1)
-    {
+int pop() {
+    if ((st.top) == -1) {
         printf("\nStack is Empty");
-    }
-    else
-    {
+    } else {
         int out;
-        out=st.arr[st.top];
-        st.top++;
+        out = st.arr[st.top];
+        st.top--;
         return out;
     }
 }
 
 //Peek
-int peek()
-{
+int peek() {
     int display;
-    display=st.arr[st.rear];
-    return display;
+    if ((st.top) == -1) {
+        printf("\nStack is empty");
+        return 0;
+    } else {
+        display = st.arr[st.top];
+        return display;
+    }
 }
 
 //Display Stack
-void display()
-{
-    if((st.top)>=0)
-    {
+void display() {
+    if ((st.top) >= 0) {
         printf("\n\nElements in the Stack");
-        for(i=st.top;i>=0;i++)
-        {
-            printf("\n%d",st.arr[i]);
+        for (int i = st.top; i >= 0; i--) {
+            printf("\n%d", st.arr[i]);
         }
-    }
-    else
-    {
+    } else {
         printf("No elements to Display");
     }
 }
 
-int main()
-{
-    st.top=-1;
+int main() {
+    st.top = -1;
     printf("Enter a Stack size less than 100 : ");
-    scanf("%d",&size);
-    printf("\nStack Operations.....");
-    printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.PEEK\n\t 4.DISPLAY\n\t 5.EXIT");
-
-    do{
+    scanf("%d", &size);
+    do {
+        printf("\nStack Operations.....");
+        printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.PEEK\n\t 4.DISPLAY\n\t 5.EXIT");
         printf("\nEnter Your Choice  ");
-        scanf("%c",&choice);
-        switch(choice)
-        {
-        case 1:
-            {
-                push(ele);break;
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1: {
+                push();
+                break;
             }
-        case 2:
-            {
-                printf("%d",pop());
+            case 2: {
+                a = pop();
+                printf("\n%d", a);
+                printf(" Deleted");
+                break;
             }
-        case 3:
-            {
-                printf("%d",peek());
+            case 3: {
+                a = peek();
+                if (st.top >= 0) {
+                    printf("%d", peek());
+                    printf(" is top most element in the stack");
+                }
+                break;
             }
-        case 4:
-            {
-                display();break;
+            case 4: {
+                display();
+                break;
             }
-        case 5:
-            {
-                printf("\n\t EXIT Point");break;
+            case 5: {
+                printf("\n\t EXIT Point");
+                break;
             }
-        default:
-            printf("\nEnter a correct choice (1,2,3,4,5)");
+            default: {
+                printf("\nEnter a correct choice (1,2,3,4,5)");
+            }
         }
-    }while(choice=5);
+    } while (choice != 5);
     return 0;
 }
